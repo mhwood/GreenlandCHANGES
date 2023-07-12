e2cdc907744bbc4cf76291c577fa64c5af7d7def
tparker1 <4665986+tparker1@users.noreply.github.com>
2023-07-12 10:36:57 -0700

    Add setup.py, move some files, and add new draft files for creating stacks

--- init_object.py
+++ init_object.py
@@ -1,4 +1,3 @@
-
 # this script defines a class used to hold all pertinent information to
 # compile the elevation and velocity
 
@@ -8,7 +7,6 @@ import os
 
 
 class GreenlandCHANGES:
-
     def __init__(self, project_folder, data_folder):
 
         #this initiates the global domain and all of its associated parameters
@@ -33,11 +31,12 @@ class GreenlandCHANGES:
     # # these are functions to set parameters to prepare the retrieval and regridding scripts
 
     def create_directory_structure(self):
-        from .toolbox.initiation import directory_construction as dc
+        from toolbox.initiation import directory_construction as dc
         dc.construct_output_directory_structure(self)
 
     def initiate_process_metadata_output(self):
-        output_summary_header = 'Processes initiated on '+str(datetime.date.today())+'\n'
+        output_summary_header = 'Processes initiated on ' + str(
+            datetime.date.today()) + '\n'
         self.output_summary += output_summary_header
         self.start_time = time.time()
 
@@ -48,8 +47,8 @@ class GreenlandCHANGES:
         self.save_outputs_summary = True
         self.delete_downloaded_files = False
 
-        self.date_1 = datetime.datetime(1900,1,1)
-        self.date_2 = datetime.datetime(2100,1,1)
+        self.date_1 = datetime.datetime(1900, 1, 1)
+        self.date_2 = datetime.datetime(2100, 1, 1)
 
         self.region_initiated = False
         self.region_name = 'untitled_region'
@@ -191,9 +190,7 @@ class GreenlandCHANGES:
         self.tandemx_username = ''
         self.tandemx_password = ''
 
-
-
-    def set_custom_extents(self,extents):
+    def set_custom_extents(self, extents):
         self.extents = extents
 
     def deactivate_all_sources(self):
@@ -229,18 +226,21 @@ class GreenlandCHANGES:
         print('    extents: ', self.extents)
         print(' ')
         print('Velocity Parameters:')
-        print('    compile_velocity: ',self.compile_velocity)
+        print('    compile_velocity: ', self.compile_velocity)
         if self.compile_velocity:
-            print('    velocity_grid_posting: ',self.velocity_grid_posting)
+            print('    velocity_grid_posting: ', self.velocity_grid_posting)
             print('    velocity_grid_epsg: ', self.velocity_grid_epsg)
             print('    create_velocity_stacks: ', self.create_velocity_stacks)
             print('    Velocity Sources:')
-            print('        compile_golive_data:',self.compile_golive_data)
-            print('        compile_measures_insar_data:', self.compile_measures_insar_data)
-            print('        compile_measure_optical_data:', self.compile_measures_optical_data)
-            print('        compile_measures_quarterly_mosaic_data:', self.compile_measures_quarterly_mosaic_data)
-            print('        compile_measures_multiyear_mosaic_data:', self.compile_measures_multiyear_mosaic_data)
-
+            print('        compile_golive_data:', self.compile_golive_data)
+            print('        compile_measures_insar_data:',
+                  self.compile_measures_insar_data)
+            print('        compile_measure_optical_data:',
+                  self.compile_measures_optical_data)
+            print('        compile_measures_quarterly_mosaic_data:',
+                  self.compile_measures_quarterly_mosaic_data)
+            print('        compile_measures_multiyear_mosaic_data:',
+                  self.compile_measures_multiyear_mosaic_data)
 
         print(' ')
         print('Elevation Parameters:')
@@ -248,36 +248,48 @@ class GreenlandCHANGES:
         if self.compile_elevation:
             print('    elevation_grid_posting: ', self.elevation_grid_posting)
             print('    elevation_grid_epsg: ', self.elevation_grid_epsg)
-            print('    create_elevation_stacks: ', self.create_elevation_stacks)
-            print('    overwrite_existing_elevation_stacks: ', self.overwrite_existing_elevation_stacks)
+            print('    create_elevation_stacks: ',
+                  self.create_elevation_stacks)
+            print('    overwrite_existing_elevation_stacks: ',
+                  self.overwrite_existing_elevation_stacks)
             print('    Elevation Sources:')
-            print('        compile_arcticDEM_data:', self.compile_arcticDEM_data)
+            print('        compile_arcticDEM_data:',
+                  self.compile_arcticDEM_data)
             print('        compile_cryosat2_data:', self.compile_cryosat2_data)
             print('        compile_gimp_data:', self.compile_gimp_data)
             print('        compile_glistin_data:', self.compile_glistin_data)
-            print('        compile_icebridge_atm_data:', self.compile_icebridge_atm_data)
-            print('        compile_icebridge_lvis_data:', self.compile_icebridge_lvis_data)
+            print('        compile_icebridge_atm_data:',
+                  self.compile_icebridge_atm_data)
+            print('        compile_icebridge_lvis_data:',
+                  self.compile_icebridge_lvis_data)
             print('        compile_icesat_data:', self.compile_icesat_data)
             print('        compile_icesat2_data:', self.compile_icesat2_data)
             print('        compile_kms_data:', self.compile_kms_data)
-            print('        compile_preicebridge_atm_data',self.compile_preicebridge_atm_data)
+            print('        compile_preicebridge_atm_data',
+                  self.compile_preicebridge_atm_data)
             print('        compile_tandemx_data:', self.compile_tandemx_data)
 
     def print_arcticDEM_parameters(self):
         print('ArcticDEM Parameters:')
         print('    compile_arcticDEM_data: ', self.compile_arcticDEM_data)
         if self.compile_arcticDEM_data:
-            print('    download_new_arcticDEM_data: ', self.download_new_arcticDEM_data)
-            print('    keep_high_resolution_arcticDEM_data: ', self.keep_high_resolution_arcticDEM_data)
-            print('    resample_high_resolution_arcticDEM_data: ', self.resample_high_resolution_arcticDEM_data)
-            print('    max_number_of_arcticDEM_files: ', self.max_number_of_arcticDEM_files)
+            print('    download_new_arcticDEM_data: ',
+                  self.download_new_arcticDEM_data)
+            print('    keep_high_resolution_arcticDEM_data: ',
+                  self.keep_high_resolution_arcticDEM_data)
+            print('    resample_high_resolution_arcticDEM_data: ',
+                  self.resample_high_resolution_arcticDEM_data)
+            print('    max_number_of_arcticDEM_files: ',
+                  self.max_number_of_arcticDEM_files)
 
     def print_cryosat2_parameters(self):
         print('CryoSat2 Parameters:')
         print('    compile_cryosat2_data: ', self.compile_cryosat2_data)
         if self.compile_cryosat2_data:
-            print('    download_new_cryosat2_data: ', self.download_new_cryosat2_data)
-            print('    max_number_of_cryosat2_files: ', self.max_number_of_cryosat2_files)
+            print('    download_new_cryosat2_data: ',
+                  self.download_new_cryosat2_data)
+            print('    max_number_of_cryosat2_files: ',
+                  self.max_number_of_cryosat2_files)
 
     def print_gimp_parameters(self):
         print('GIMP Parameters:')
@@ -287,64 +299,71 @@ class GreenlandCHANGES:
         print('GLISTIN-A Parameters:')
         print('    compile_glistin_data: ', self.compile_glistin_data)
         if self.compile_glistin_data:
-            print('    download_new_glistin_data: ', self.download_new_glistin_data)
-            print('    max_number_of_glistin_files: ', self.max_number_of_glistin_files)
-            print('    podaac_username: ',self.podaac_username)
+            print('    download_new_glistin_data: ',
+                  self.download_new_glistin_data)
+            print('    max_number_of_glistin_files: ',
+                  self.max_number_of_glistin_files)
+            print('    podaac_username: ', self.podaac_username)
             print('    podaac_password: ', self.podaac_password)
 
     def print_icebridge_parameters(self):
         print('Operation Icebridge Parameters:')
         print('    compile_icebridge_data: ', self.compile_icebridge_data)
         if self.compile_icebridge_data:
-            print('    download_new_icebridge_data: ', self.download_new_icebridge_data)
-            print('    max_number_of_icebridge_files: ', self.max_number_of_icebridge_files)
+            print('    download_new_icebridge_data: ',
+                  self.download_new_icebridge_data)
+            print('    max_number_of_icebridge_files: ',
+                  self.max_number_of_icebridge_files)
 
     def print_icesat_parameters(self):
         print('ICESat Parameters:')
         print('    compile_icesat_data: ', self.compile_icesat_data)
         if self.compile_icesat_data:
-            print('    download_new_icesat_data: ', self.download_new_icesat_data)
-            print('    max_number_of_icesat_files: ', self.max_number_of_icesat_files)
+            print('    download_new_icesat_data: ',
+                  self.download_new_icesat_data)
+            print('    max_number_of_icesat_files: ',
+                  self.max_number_of_icesat_files)
 
     def print_icesat2_parameters(self):
         print('ICESat-2 Parameters:')
         print('    compile_icesat2_data: ', self.compile_icesat2_data)
         if self.compile_icesat2_data:
-            print('    download_new_icesat2_data: ', self.download_new_icesat2_data)
-            print('    max_number_of_icesat2_files: ', self.max_number_of_icesat2_files)
+            print('    download_new_icesat2_data: ',
+                  self.download_new_icesat2_data)
+            print('    max_number_of_icesat2_files: ',
+                  self.max_number_of_icesat2_files)
 
     def print_kms_parameters(self):
         print('KMS Parameters:')
         print('    compile_kms_data: ', self.compile_kms_data)
         if self.compile_kms_data:
             print('    kms_output_file: ', self.kms_output_file)
-            print('    save_tif_copy_of_kms_dem: ', self.save_tif_copy_of_kms_dem)
+            print('    save_tif_copy_of_kms_dem: ',
+                  self.save_tif_copy_of_kms_dem)
 
     def print_sentinel3_parameters(self):
         print('Sentinel-3 Parameters:')
         print('    compile_sentinel3_data: ', self.compile_sentinel3_data)
         if self.compile_sentinel3_data:
-            print('    download_new_sentinel3_data: ', self.download_new_sentinel3_data)
-            print('    max_number_of_sentinel3_files: ', self.max_number_of_sentinel3_files)
+            print('    download_new_sentinel3_data: ',
+                  self.download_new_sentinel3_data)
+            print('    max_number_of_sentinel3_files: ',
+                  self.max_number_of_sentinel3_files)
 
     def print_tandemx_parameters(self):
         print('ICESat-2 Parameters:')
         print('    compile_tandemx_data: ', self.compile_tandemx_data)
         if self.compile_tandemx_data:
-            print('    download_new_tandemx_data: ', self.download_new_tandemx_data)
-            print('    max_number_of_tandemx_files: ', self.max_number_of_tandemx_files)
-
-
-
-
-
-
+            print('    download_new_tandemx_data: ',
+                  self.download_new_tandemx_data)
+            print('    max_number_of_tandemx_files: ',
+                  self.max_number_of_tandemx_files)
 
     # ####################################################################################################################
     # # these are functions to set parameters to run the retrieval and regridding scripts
 
     def check_region_intiated(self):
-        if len(self.extents)==4:
+        if len(self.extents) == 4:
             extents_initiated = True
         else:
             extents_initiated = False
@@ -356,21 +375,24 @@ class GreenlandCHANGES:
         if extents_initiated and region_name_initiated:
             self.region_initiated = True
 
-
     def initiate_velocity_grids(self):
-        from .toolbox.initiation import grid_construction as gc
+        from toolbox.initiation import grid_construction as gc
         gc.create_velocity_grids_from_extents(self)
 
     def initiate_elevation_grids(self):
-        from .toolbox.initiation import grid_construction as gc
+        from toolbox.initiation import grid_construction as gc
         gc.create_elevation_grids_from_extents(self)
 
     def write_process_metadata_output(self):
         self.end_time = time.time()
         total_time = self.end_time - self.start_time
-        self.output_summary+='\n\nTotal time elapsed for compilation: '+str(datetime.timedelta(seconds=total_time))
+        self.output_summary += '\n\nTotal time elapsed for compilation: ' + str(
+            datetime.timedelta(seconds=total_time))
 
-        f=open(os.path.join(self.project_folder,self.region_name,self.region_name+' CHANGES Process Metadata.txt'),'w')
+        f = open(
+            os.path.join(self.project_folder, self.region_name,
+                         self.region_name + ' CHANGES Process Metadata.txt'),
+            'w')
         f.write(self.output_summary)
         f.close()
 
@@ -378,7 +400,9 @@ class GreenlandCHANGES:
         self.check_region_intiated()
         if not self.region_initiated:
             print('!! Alert !!')
-            print('!! Please define a region name and valid extents before running the compilation !!')
+            print(
+                '!! Please define a region name and valid extents before running the compilation !!'
+            )
             print('   There are two options:')
             print('   Option 1: Pre-defined glacier extent:')
             print('      GC.set_extents_by_glacier([glacier name here])')
@@ -388,18 +412,16 @@ class GreenlandCHANGES:
         else:
             self.create_directory_structure()
 
-
             if self.compile_velocity:
                 if len(self.velocity_grid_x) < 1:
                     self.initiate_velocity_grids()
-                from .changes.velocity import velocity_compilation as vc
+                from changes.velocity import velocity_compilation as vc
                 vc.download_and_regrid_velocity_data(self)
 
             if self.compile_elevation:
                 if len(self.elevation_grid_x) < 1:
                     self.initiate_elevation_grids()
-                from .changes.elevation import elevation_compilation as ec
+                from changes.elevation import elevation_compilation as ec
                 ec.download_and_regrid_elevation_data(self)
 
             self.write_process_metadata_output()
-
---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='changes-toolbox',
+      version='1.0',
+      description='Modules for CHANGES',
+      author='Tara Parker',
+      author_email='tara.parker@sjsu.edu',
+      maintainer='Mike Wood',
+      maintainer_email='mike.wood@sjsu.edu',
+      url='https://github.com/mhwood/GreenlandCHANGES/tree/master',
+      packages=['toolbox']
+     )
